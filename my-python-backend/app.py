eacd5d25d9ec1330de0791bed8cd6076bb116d8b
jimjrxieb <jimmie012506@gmail.com>
2024-08-02 12:51:38 -0400

    fastapi change not flask

--- my-python-backend/app.py
+++ my-python-backend/app.py
@@ -1,12 +1,4 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    # Your business logic here (e.g., fetch users from a database)
-    users = [{'id': 1, 'name': 'Frank'}, {'id': 2, 'name': 'Constant'}]
-    return jsonify(users)
-
-if __name__ == '__main__':
-    app.run(debug=True)
