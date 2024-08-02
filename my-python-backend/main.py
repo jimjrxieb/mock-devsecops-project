f267806c6fcef71d192253b5db2c4d005b1285f8
jimjrxieb <jimmie012506@gmail.com>
2024-08-02 13:22:02 -0400

    mongoDB added

--- my-python-backend/main.py
+++ my-python-backend/main.py
@@ -10,3 +10,8 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {'item_id': item_id, 'q': q}
 
+@app.route('/users', methods=['GET'])
+def get_users_from_db():
+    users_collection = db['users']
+    users = list(users_collection.find({}, {'_id': 0}))
+    return jsonify(users)
